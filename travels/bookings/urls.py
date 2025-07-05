26c3a59cdc49b4f2d26c1e55a23797d94795097c
Vaishnavireddiee01 <vanugu47@gmail.com>
2025-07-06 02:05:57 +0530

    Update URL routing for user bookings and add CORS headers to settings

--- travels/bookings/urls.py
+++ travels/bookings/urls.py
@@ -7,6 +7,6 @@ urlpatterns = [
     path('buses/', BusListCreateView.as_view(), name='bus-list'),
     path('register/', RegisterView.as_view(), name='register'),
     path('login/', LoginView.as_view(), name='login'),
-    path('user-bookings', UserBookingView.as_view(), name='user-bookings'),
+    path('user/<int:user_id>/bookings/', UserBookingView.as_view(), name='user-bookings'),
     path('booking/', BookingView.as_view(), name='booking'),
 ]
